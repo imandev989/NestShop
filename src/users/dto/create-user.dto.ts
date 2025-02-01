4411e557619e09f88cf843dd58be3614138ccbba
iman.dev989 <iman.dev989@gmail.com>
2025-02-01 19:58:33 +0330

    dto and validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsString, Length, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(11, 11, { message: 'mobile number must be 11 characters' })
+  @Matches(/^[0-9]{11}$/, { message: 'mobile number must be numeric' })
+  mobile: string;
+  display_name: string;
+  password: string;
+  role: string;
+}
