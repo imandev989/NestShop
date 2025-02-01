2b0f5db4535bdcd07ca9a1d3d2643d5a5b9a51d9
iman.dev989 <iman.dev989@gmail.com>
2025-02-01 20:06:58 +0330

    complete validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,36 @@
-import { IsString, Length, Matches } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+  //   Matches,
+  MinLength,
+} from 'class-validator';
+import UserRoleEnum from '../enums/userRoleEnum';
 
 export class CreateUserDto {
-  @IsString()
+  @IsString({ message: 'mobile number must be a string' })
   @Length(11, 11, { message: 'mobile number must be 11 characters' })
-  @Matches(/^[0-9]{11}$/, { message: 'mobile number must be numeric' })
+  //   @Matches(/^[0-9]{11}$/, { message: 'mobile number must be numeric' })
+  @IsNotEmpty({ message: 'mobile number is required' })
+  @Transform(({ value }) => value.trim())
   mobile: string;
+
+  @IsNotEmpty({ message: 'display name is required' })
+  @IsString({ message: 'display name must be a string' })
   display_name: string;
+
+  @IsString({ message: 'password must be a string' })
+  @IsOptional()
+  @MinLength(8, { message: 'password must be at least 8 characters' })
+  //   @IsNotEmpty({ message: 'password is required' })
   password: string;
-  role: string;
+
+  @IsEnum(UserRoleEnum, {
+    message: 'role must be either user, admin or superAdmin',
+  })
+  @IsOptional()
+  role: UserRoleEnum;
 }
