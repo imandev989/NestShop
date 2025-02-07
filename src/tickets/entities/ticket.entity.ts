0169c32ea2c4c48255a9901f49b7bf714fb97481
iman.dev989 <iman.dev989@gmail.com>
2025-02-07 22:51:02 +0330

    create ticket service

---
+++ src/tickets/entities/ticket.entity.ts
@@ -0,0 +1,25 @@
+import { User } from 'src/users/entities/user.entity';
+import { Column, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Ticket {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  subject: string;
+
+  @Column()
+  description: string;
+
+  @ManyToOne(() => User, (user) => user.tickets)
+  user: User;
+
+  @ManyToOne(() => Ticket, (ticket) => ticket.replies, { nullable: true })
+  replyTo: Ticket;
+
+  @OneToMany(() => Ticket, (ticket) => ticket.replyTo)
+  replies: Ticket[];
+}
---
+++ src/tickets/tickets.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTicketDto } from './dto/create-ticket.dto';
+import { UpdateTicketDto } from './dto/update-ticket.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Ticket } from './entities/ticket.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class TicketsService {
+  constructor(
+    @InjectRepository(Ticket)
+    private readonly ticketRepository: Repository<Ticket>,
+    private readonly userService: UsersService,
+  ) {}
+  async create(createTicketDto: CreateTicketDto): Promise<Ticket> {
+    const { userId, replyTo, ...TicketDto } = createTicketDto;
+    const user = await this.userService.findOne(userId);
+    const replyToTicket = await this.ticketRepository.findOneByOrFail({
+      id: replyTo,
+    });
+    const ticket = this.ticketRepository.create({
+      ...TicketDto,
+      user,
+      replyTo: replyToTicket,
+    });
+    return this.ticketRepository.save(ticket);
+  }
+
+  // findAll() {
+  //   return `This action returns all tickets`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} ticket`;
+  // }
+
+  // update(id: number, updateTicketDto: UpdateTicketDto) {
+  //   return `This action updates a #${id} ticket`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} ticket`;
+  // }
+}
