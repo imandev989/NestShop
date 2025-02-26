df1d36198963d35dfa57a358ea6e719249c5a54d
iman.dev989 <iman.dev989@gmail.com>
2025-02-26 17:57:04 +0330

    add all remove senario

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Res,
+  HttpStatus,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { Response } from 'express';
@@ -28,6 +37,36 @@ export class CategoriesController {
       statusCode: HttpStatus.OK,
       data: categories,
       message: 'categories fetched successfully',
+    });
+  }
+
+  @Delete('remove-only-category/:id')
+  async removeOnly(@Param('id') id: string, @Res() res: Response) {
+    await this.categoriesService.removeOnlyCategory(+id);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'The selected category has been successfully deleted',
+    });
+  }
+
+  @Delete('safe-remove/:id')
+  async safeRemove(@Param('id') id: string, @Res() res: Response) {
+    await this.categoriesService.safeRemove(+id);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'The selected category has been successfully deleted',
+    });
+  }
+
+  @Delete('remove/:id')
+  async remove(@Param('id') id: string, @Res() res: Response) {
+    await this.categoriesService.remove(+id);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'The selected category has been successfully deleted',
     });
   }
 }
